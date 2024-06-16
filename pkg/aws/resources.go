93a23d2b027a55814ab1e99d65cbc7c47501fd9f
himanshuvaish <30329562+himanshuvaish@users.noreply.github.com>
2024-06-16 14:25:03 +0530

    Update resources.go
    use v6 aws

--- pkg/aws/resources.go
+++ pkg/aws/resources.go
@@ -7,8 +7,8 @@ import (
 	code2cloudv1deploybktstackawsmodel "github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/storagebucket/stack/aws/model"
 	pulumiawsprovider "github.com/plantoncloud/pulumi-stack-runner-go-sdk/pkg/automation/provider/aws"
 	puluminameoutputaws "github.com/plantoncloud/pulumi-stack-runner-go-sdk/pkg/name/provider/cloud/aws/output"
-	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws"
-	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -32,7 +32,7 @@ func (s *ResourceStack) Resources(ctx *pulumi.Context) error {
 func addBucket(ctx *pulumi.Context, input *code2cloudv1deploybktstackawsmodel.StorageBucketAwsStackResourceInput,
 	tags map[string]string, awsProvider *aws.Provider) error {
 	addedBucket, err := s3.NewBucket(ctx, input.StorageBucket.Metadata.Name, &s3.BucketArgs{
-		Acl:          pulumi.String("private"),
+		Acl:          pulumi.String(s3.CannedAclPrivate), // Set the bucket ACL to private
 		Tags:         pulumi.ToStringMap(tags),
 		Region:       pulumi.String(input.StorageBucket.Spec.Aws.Region),
 		Bucket:       pulumi.String(input.StorageBucket.Metadata.Name),
